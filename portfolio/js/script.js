b88d113ef07c51a7711e8ca46cf39dbc8416dc95
meet kathiriya <142255158+morphs7623@users.noreply.github.com>
2024-04-22 12:53:37 +0530

    Add files via upload

---
+++ portfolio/js/script.js
@@ -0,0 +1,35 @@
+	
+    var typed = new Typed('#element', {
+        strings: ['Front-End Developer'],
+        typeSpeed: 50,
+      });
+    
+    // Sticky Navbar
+    let header = document.querySelector('header');
+    let menu = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+     
+     
+    window.addEventListener('scroll', () => {
+        header.classList.toggle('shadow', window.scrollY > 0);
+    });
+     
+    menu.onclick = () => {
+        navbar.classList.toggle('active');
+    }
+    window.onscroll = () => {
+        navbar.classList.remove('active');
+    }
+     
+    // Dark Mode
+    let darkmode = document.querySelector('#darkmode');
+     
+    darkmode.onclick = () => {
+        if(darkmode.classList.contains('bx-moon')){
+            darkmode.classList.replace('bx-moon','bx-sun');
+            document.body.classList.add('active');
+        }else{
+            darkmode.classList.replace('bx-sun','bx-moon');
+            document.body.classList.remove('active');
+        }
+    }
